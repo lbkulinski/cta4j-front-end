17d9140cae33a7923c6c560a3d3f53f9ed115a68
Logan Kulinski <rashes_lineage02@icloud.com>
2023-04-23 11:03:57 -0500

    Front-end changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    Box,
+    FormControl,
+    InputLabel,
+    MenuItem, Paper,
+    Select,
+    Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow
+} from "@mui/material";
+import {useQuery} from '@apollo/client';
+import { gql } from './__generated__';
+
+const GET_STATIONS = gql(`
+query GetStations {
+    getStations {
+        id
+        name
+    }
+}
+`);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [station, setStation] = React.useState(null);
+
+    const { loading, error, data } = useQuery(GET_STATIONS);
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    if (error) {
+        return <p>Error : {error.message}</p>;
+    }
+
+    if (!data) {
+        return <p>Error...</p>
+    }
+
+    const stations = data.getStations;
+
+    if (!stations) {
+        return <p>Error...</p>
+    }
+
+    return (
+        <div>
+            <Stack spacing={2}>
+                <FormControl sx={{m: 2, maxWidth: 500}} size="small">
+                    <InputLabel>
+                        Station
+                    </InputLabel>
+                    <Select autoWidth>
+                        {
+                            stations.map(station => {
+                                return (
+                                    <MenuItem value={station.id}>
+                                        {
+                                            station.name
+                                        }
+                                    </MenuItem>
+                                );
+                            })
+                        }
+                    </Select>
+                </FormControl>
+                <Box sx={{p: 2}}>
+                    <TableContainer component={Paper}>
+                        <Table size="small">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Route</TableCell>
+                                    <TableCell>Destination</TableCell>
+                                    <TableCell>Run</TableCell>
+                                    <TableCell>ETA</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Box>
+            </Stack>
+        </div>
+    );
 }
 
 export default App;
