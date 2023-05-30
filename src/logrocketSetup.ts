536214ef969afa5f0ef1f985fd07e2b09791f65a
Logan Kulinski <rashes_lineage02@icloud.com>
2023-05-30 14:30:52 -0500

    Added LogRocket support

--- src/logrocketSetup.ts
+++ src/logrocketSetup.ts
@@ -1,3 +1,7 @@
 import LogRocket from 'logrocket';
+// @ts-ignore
+import setupLogRocketReact from 'logrocket-react';
 
 LogRocket.init(import.meta.env.VITE_LOGROCKET_APP_ID);
+
+setupLogRocketReact(LogRocket);
