8bb6f8ccede3bf94079075ff95f3d0fa6bc9ce1d
Logan Kulinski <rashes_lineage02@icloud.com>
2023-06-07 23:45:35 -0500

    Adding bus support

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-    schema: `https://main--logan-kulinskis-team-w5zqh7.apollographos.net/graphql`,
+    schema: `http://localhost:8080/graphql`,
     documents: ['src/**/*.tsx'],
     generates: {
         './src/__generated__/': {
