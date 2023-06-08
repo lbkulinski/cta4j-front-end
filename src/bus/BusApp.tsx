aa56d8b471bdf2c906abd4c2d64c1302c867b8f6
Logan Kulinski <rashes_lineage02@icloud.com>
2023-06-07 23:44:53 -0500

    Adding bus support

--- src/bus/BusApp.tsx
+++ src/bus/BusApp.tsx
@@ -3,30 +3,41 @@ import '../App.css';
 import Routes from "./Routes.tsx";
 import Directions from "./Directions.tsx";
 import Stops from "./Stops.tsx";
+import Buses from "./Buses.tsx";
 
 function BusApp() {
-    /*
     const searchParams = new URLSearchParams(window.location.search);
 
-    let defaultStationId = searchParams.get("stationId");
+    let defaultRouteId = searchParams.get("routeId");
 
-    if (defaultStationId === null) {
-        defaultStationId = localStorage.getItem("stationId");
+    if (defaultRouteId === null) {
+        defaultRouteId = localStorage.getItem("routeId");
     }
 
-    const [stationId, setStationId] = React.useState<string | null>(defaultStationId);
-     */
-    const [routeId, setRouteId] = React.useState<string | null>(null);
+    let defaultDirection = searchParams.get("direction");
 
-    const [direction, setDirection] = React.useState<string | null>(null);
+    if (defaultDirection === null) {
+        defaultDirection = localStorage.getItem("direction");
+    }
 
-    const [stopId, setStopId] = React.useState<string | null>(null);
+    let defaultStopId = searchParams.get("stopId");
+
+    if (defaultStopId === null) {
+        defaultStopId = localStorage.getItem("stopId");
+    }
+
+    const [routeId, setRouteId] = React.useState<string | null>(defaultRouteId);
+
+    const [direction, setDirection] = React.useState<string | null>(defaultDirection);
+
+    const [stopId, setStopId] = React.useState<string | null>(defaultStopId);
 
     return (
         <div>
             <Routes routeId={routeId} setRouteId={setRouteId} />
             <Directions routeId={routeId} direction={direction} setDirection={setDirection} />
             <Stops routeId={routeId} direction={direction} stopId={stopId} setStopId={setStopId} />
+            <Buses routeId={routeId} stopId={stopId} />
         </div>
     );
 }
---
+++ src/bus/Buses.tsx
@@ -0,0 +1,196 @@
+import {Alert, Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {gql} from "../__generated__";
+import {useQuery} from "@apollo/client";
+import {useRollbar} from "@rollbar/react";
+
+interface BusesProps {
+    routeId: string | null,
+    stopId: string | null
+}
+
+const GET_BUSES = gql(`
+query GetBuses($routeId: ID!, $stopId: ID!) {
+    getBuses(routeId: $routeId, stopId: $stopId) {
+        id
+        type
+        stop
+        route
+        destination
+        delayed
+        eta
+    }
+}
+`);
+
+interface Bus {
+    id: string,
+    type: string,
+    stop: string,
+    route: string,
+    destination: string,
+    delayed: boolean,
+    eta: number
+}
+
+function getRow(bus: Bus) {
+    const key = JSON.stringify(bus);
+
+    let rowStyles = {};
+
+    const eta = bus.eta;
+
+    if (eta <= 1) {
+        rowStyles = {
+            backgroundColor: "#13251f"
+        };
+    } else if (bus.delayed) {
+        rowStyles = {
+            backgroundColor: "#381717"
+        }
+    }
+
+    const etaString = (eta <= 1) ? "Due" : `${eta} min`;
+
+    return (
+        <TableRow key={key} sx={rowStyles}>
+            <TableCell>
+                {
+                    bus.id
+                }
+            </TableCell>
+            <TableCell>
+                {
+                    bus.type
+                }
+            </TableCell>
+            <TableCell>
+                {
+                    bus.destination
+                }
+            </TableCell>
+            <TableCell>
+                {
+                    etaString
+                }
+            </TableCell>
+        </TableRow>
+    );
+}
+
+function getTable(buses: Bus[] | null) {
+    if (buses === null) {
+        return null;
+    }
+
+    return (
+        <Box sx={{p: 2}}>
+            <TableContainer component={Paper}>
+                <Table size="small">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell sx={{fontWeight: "bold"}}>ID</TableCell>
+                            <TableCell sx={{fontWeight: "bold"}}>Type</TableCell>
+                            <TableCell sx={{fontWeight: "bold"}}>Destination</TableCell>
+                            <TableCell sx={{fontWeight: "bold"}}>ETA</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            buses.map((bus) => getRow(bus))
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
+    );
+}
+
+function compareBuses(bus0: Bus, bus1: Bus) {
+    const route0 = bus0.route;
+
+    const destination0 = bus0.destination;
+
+    const eta0 = bus0.eta;
+
+    const route1 = bus1.route;
+
+    const destination1 = bus1.destination;
+
+    const eta1 = bus1.eta;
+
+    if (route0 < route1) {
+        return -1;
+    } else if (route0 > route1) {
+        return 1;
+    } else if (destination0 < destination1) {
+        return -1;
+    } else if (destination0 > destination1) {
+        return 1;
+    } else if (eta0 < eta1) {
+        return -1;
+    } else if (eta0 === eta1) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+function Buses(props: BusesProps) {
+    const routeId = props.routeId;
+
+    if (routeId === null) {
+        return null;
+    }
+
+    const stopId = props.stopId;
+
+    if (stopId === null) {
+        return null;
+    }
+
+    const options = {
+        variables: {
+            routeId: routeId,
+            stopId: stopId
+        }
+    }
+
+    const {loading, error, data, startPolling} = useQuery(GET_BUSES, options);
+
+    if (loading) {
+        return null;
+    }
+
+    if (error) {
+        const rollbar = useRollbar();
+
+        const errorData = {
+            error: error,
+            data: data
+        }
+
+        const errorDataString = JSON.stringify(errorData);
+
+        rollbar.error("An error occurred when trying to fetch the buses", errorDataString);
+
+        return (
+            <Alert severity="error">
+                Error: The buses could not be loaded. Please refresh the page or try again later.
+            </Alert>
+        );
+    }
+
+    if (!data) {
+        return null;
+    }
+
+    startPolling(60000);
+
+    const buses = Array.from(data.getBuses);
+
+    buses.sort(compareBuses);
+
+    return getTable(buses);
+}
+
+export default Buses;
