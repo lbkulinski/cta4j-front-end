47cc93cdb73d58306033553373decda80e08f302
Logan Kulinski <rashes_lineage02@icloud.com>
2023-12-17 15:35:42 -0600

    Remove holiday train

--- src/holiday-train/HolidayApp.tsx
+++ src/holiday-train/HolidayApp.tsx
@@ -1,15 +1,10 @@
 import '../App.css';
-import HolidayTrain from "./HolidayTrain.tsx";
 import Box from "@mui/material/Box";
 import HolidayBus from "./HolidayBus.tsx";
 
 function HolidayApp() {
     return (
         <div>
-            <Box sx={{p: 2}}>
-                <h2 style={{color: "#B3000C"}}>Holiday Train &#127877;&#128647;</h2>
-            </Box>
-            <HolidayTrain />
             <Box sx={{p: 2}}>
                 <h2 style={{color: "#B3000C"}}>Holiday Bus &#127877;&#128652;</h2>
             </Box>
